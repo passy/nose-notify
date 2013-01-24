17d8f5ea3c75559a8e992a4d2dfe595279cd1580
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-24 15:59:51 -0200

    Fallback to notify-send when pynotify not available

---
+++ tests/test_adapters.py
@@ -0,0 +1,66 @@
+import os
+from unittest import TestCase
+
+from mock import patch
+from nose.tools import istest
+
+from nosenotify.adapters import get_icon, init, Notification
+
+
+class HelpersTest(TestCase):
+    @istest
+    def gets_an_info_icon(self):
+        icon = get_icon('dialog-info')
+
+        self.assertTrue(os.path.isfile(icon))
+
+    @istest
+    def gets_a_warning_icon(self):
+        icon = get_icon('dialog-warning')
+
+        self.assertTrue(os.path.isfile(icon))
+
+    @istest
+    def differentiates_between_info_and_warning(self):
+        info = get_icon('dialog-info')
+        warning = get_icon('dialog-warning')
+
+        self.assertNotEqual(info, warning)
+
+    @istest
+    def inits_pynotify(self):
+        init('some message')
+
+
+class NotificationTest(TestCase):
+    @istest
+    @patch('commands.getoutput')
+    @patch('nosenotify.adapters.get_icon')
+    def shows_a_notification_with_icon(self, get_icon, getoutput):
+        get_icon.return_value = 'some-icon.png'
+
+        notification = Notification('some title', 'some body', 'some-icon')
+        notification.show()
+
+        getoutput.assert_called_with(
+            'notify-send --icon="some-icon.png" "some title" "some body"')
+
+    @istest
+    @patch('commands.getoutput')
+    @patch('nosenotify.adapters.get_icon')
+    def shows_a_notification_without_icon(self, get_icon, getoutput):
+        notification = Notification('some title', 'some body')
+        notification.show()
+
+        getoutput.assert_called_with(
+            'notify-send "some title" "some body"')
+        self.assertFalse(get_icon.called)
+
+    @istest
+    @patch('commands.getoutput')
+    @patch('nosenotify.adapters.get_icon')
+    def doesnt_trigger_if_not_shown(self, get_icon, getoutput):
+        notification = Notification('some title', 'some body')
+
+        self.assertFalse(getoutput.called)
+        self.assertFalse(get_icon.called)
